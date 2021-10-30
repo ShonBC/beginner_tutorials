bf24da3a82779559e73ab276ed4d2383071dbaf0
Shon Cortes <scortes3@umd.edu>
2021-10-30 15:45:42 -0700

    Add bash files from cpp lint/check and vlagrind

---
+++ run_cppcheck.sh
@@ -0,0 +1,2 @@
+cppcheck --enable=all --std=c++11 --language=c++ -I include/ --suppress=missingIncludeSystem $( find . -name *.cpp | grep -vE -e "^./build/" -e "^./vendor/")  $( find . -name *.h | grep -vE -e "^./build/" -e "^./vendor/")  --output-file=results/cppcheck_process.txt > results/cppcheck_result.txt
+echo "Done Processing. Results are stored in results/cppcheck_process.txt, results/cppcheck_result.txt"
