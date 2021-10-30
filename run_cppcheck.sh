98562ea388d30a5c766021823a9cf706fb73fe66
Shon Cortes <scortes3@umd.edu>
2021-10-30 16:44:22 -0700

    Remove check on .h files

--- run_cppcheck.sh
+++ run_cppcheck.sh
@@ -1,2 +1,2 @@
-cppcheck --enable=all --std=c++11 --language=c++ -I include/ --suppress=missingIncludeSystem $( find . -name *.cpp | grep -vE -e "^./build/" -e "^./vendor/")  $( find . -name *.h | grep -vE -e "^./build/" -e "^./vendor/")  --output-file=results/cppcheck_process.txt > results/cppcheck_result.txt
+cppcheck --enable=all --std=c++11 --language=c++ -I include/ --check-config --suppress=missingIncludeSystem $( find . -name *.cpp | grep -vE -e "^./build/" -e "^./vendor/") --output-file=results/cppcheck_process.txt > results/cppcheck_result.txt
 echo "Done Processing. Results are stored in results/cppcheck_process.txt, results/cppcheck_result.txt"
