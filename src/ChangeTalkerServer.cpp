81c70cf97b379c6f6d18f63763906e045c9601ef
Shon Cortes <scortes3@umd.edu>
2021-11-02 22:53:40 -0700

    Create server to take request and return response

---
+++ src/ChangeTalkerServer.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file ChangeTalkerServer.cpp
+ * @author Shon Cortes scortes3@umd.edu
+ * @brief Change the message a Publisher produces
+ * @version 0.1
+ * @date 2021-11-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "ros/ros.h"
+#include "beginner_tutorials/ChangeTalker.h"
+
+bool ChangeString(beginner_tutorials::ChangeTalker::Request &req,
+                        beginner_tutorials::ChangeTalker::Response &res) {
+    res.new_string = req.desired_string;
+
+    ROS_INFO_STREAM("Request: " << req.desired_string <<
+                    " Response: " << res.new_string);
+
+    return true;
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "change_talker_server");
+    ros::NodeHandle n;
+    ros::ServiceServer service = n.advertiseService("change_talker",
+                                                        ChangeString);
+    ROS_INFO_STREAM("Ready to change the publish message.");
+    ros::spin();
+    return 0;
+}
